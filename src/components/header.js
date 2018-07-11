239b0fb97a831f9b09a0b8092a1abe8dfe379cbf
William Park <wjypark94@gmail.com>
2018-07-11 16:05:08 -0700

    added header and info section styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import TopNav from './top-nav';
-
+import './styles/header.css';
 
 export default function Header(props) {
   return (
--- src/components/info-section.js
+++ src/components/info-section.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-
+import './styles/info-section.css';
 
 export default function InfoSection(props) {
   return (
