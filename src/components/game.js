4f6fced8a0c18a3c6a5693b76285eda9a4ee7cc3
William Park <wjypark94@gmail.com>
2018-07-11 17:59:08 -0700

    added credit info

--- src/components/game.js
+++ src/components/game.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
 import Header from './header';
 import GuessSection from './guess-section';
 import StatusSection from './status-section';
+import './styles/game.css';
 
 export default function Game(props) {
   return (
@@ -12,6 +12,10 @@ export default function Game(props) {
         <GuessSection />
         <StatusSection />
       </main>
+      <p className="made-text">
+        <span>Made with </span>
+        <i className="fa fa-heart fa created-by-icon" aria-hidden="true" /> By Will Park
+      </p>
     </div>
   );
 }
